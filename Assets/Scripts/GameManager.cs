f11e0305438656d4b2f59c1e57a7c7864d180242
Alex Davies <alex@frogtrade.com>
2023-02-10 23:58:45 +0000

    Fixed Gravity

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private readonly Dictionary<Type, Component> _globals = new();
+
+    public T GetGlobalComponent<T>() where T : Component
+    {
+        if (_globals.ContainsKey(typeof(T))) _globals.Add(typeof(T), FindObjectOfType<T>());
+
+        return (T)_globals[typeof(T)];
+    }
 }
